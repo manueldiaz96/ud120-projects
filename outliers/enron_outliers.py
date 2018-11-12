fbd31d264c527a6192860d427e352da9b3f6b912
Manuel Alejandro Diaz Zapata <manuel.alejandro.diaz.zapata@gmail.com>
2018-11-12 15:28:26 -0500

    Finished Outliers Chapter

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -15,5 +15,31 @@ data = featureFormat(data_dict, features)
 
 ### your code below
 
+for key, value in data_dict.items():
+    if value['bonus'] == data.max():
+        print (key, data.max())
+        outlier = key
+
+data_dict.pop(outlier, 0)
+
+data = featureFormat(data_dict, features)
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
 
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+
+
+
+biggest = [0,0]
+for key, value in data_dict.items():
+    if value['bonus'] > 0.4e7 and value['salary'] > 1e6 and value['bonus'] !='NaN':
+    	print key 
+
+
+matplotlib.pyplot.show()
